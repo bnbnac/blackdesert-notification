95e737f6ce61b0597ca95283df1bbbb87b4b0e08
bnbnac <bnbnac@naver.com>
2023-06-01 17:13:17 +0900

    feat: build a set of PatchNote with crawled data, consider only today's and yesterday's patchnote

--- src/main/java/bnbnac/discordBot/MyCrawler.java
+++ src/main/java/bnbnac/discordBot/MyCrawler.java
@@ -6,34 +6,43 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MyCrawler {
+    public Set<PatchNote> crawl() {
+        Set<PatchNote> curPatchNotes = new HashSet<>();
+        boolean state;
 
-    // give parameter to store date
-    // 다 가져올 필요는 없다 먼저 비교한다
-    public static void crawl() {
-        // try catch?
         try {
             String url = "https://www.kr.playblackdesert.com/ko-kr/News/Notice?boardType=2";
             Document doc = Jsoup.connect(url).get();
+            Elements patchNoteElements = doc.select("strong.title");
 
-            Elements childElements = doc.select("strong.title");
-
-            for (Element child : childElements) {
-                Element date = child.select("span.date").first();
-                String dateString = date.text();
-
-                Element a = child.parent().parent().selectFirst("a");
-                String link = a.attr("href");
-
-                Element lineClamp = child.select("span.line_clamp").first();
-                String title = lineClamp.text();
+            for (Element patchNoteElement : patchNoteElements) {
+                buildCurPatchNotes(patchNoteElement, curPatchNotes);
             }
-
         } catch (IOException e) {
-            // 디코방에 announce. 리패치 성공하면 다시 announce. 실패하면 그대로.
-            // <em class="thum_cate cate_update">[업데이트]&nbsp;</em>
+            // 디코방에 announce. 리패치 성공하면 다시 announce. 실패하면 pass. state
             e.printStackTrace();
+        }
+
+        return curPatchNotes;
+    }
+
+    public void buildCurPatchNotes(Element patchNoteElement, Set<PatchNote> curPatchNotes) {
+        String date = patchNoteElement.select("span.date").first().text();
+        String link = patchNoteElement.parent().parent().selectFirst("a").attr("href");
+        String title = patchNoteElement.select("span.line_clamp").first().text();
+
+        LocalDate rawToday = LocalDate.now();
+        String today = rawToday.format(DateTimeFormatter.ofPattern("yyyy.MM.dd"));
+        String yesterday = rawToday.minusDays(1).format(DateTimeFormatter.ofPattern("yyyy.MM.dd"));
+
+        if (today.equals(date) || yesterday.equals(date)) {
+            curPatchNotes.add(new PatchNote(date, link, title));
         }
     }
 }
