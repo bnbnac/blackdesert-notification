7f66509eaab9ba0ae25f661b40f7c37e120961df
bnbnac <bnbnac@naver.com>
2023-06-01 17:10:38 +0900

    feat: build patchnote object which has date, link, title

---
+++ src/main/java/bnbnac/discordBot/PatchNote.java
@@ -0,0 +1,13 @@
+package bnbnac.discordBot;
+
+public class PatchNote {
+    public String date;
+    public String link;
+    public String title;
+
+    public PatchNote(String date, String link, String title) {
+        this.date = date;
+        this.link = link;
+        this.title = title;
+    }
+}
