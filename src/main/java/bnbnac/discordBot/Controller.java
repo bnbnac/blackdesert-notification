1b8c2174e734aef16d4267e238bdd2e948f83b79
bnbnac <bnbnac@naver.com>
2023-06-02 09:40:19 +0900

    일단 대충 끝냄

--- src/main/java/bnbnac/discordBot/Controller.java
+++ src/main/java/bnbnac/discordBot/Controller.java
@@ -1,7 +1,6 @@
 package bnbnac.discordBot;
 
 import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 
 import java.util.List;
@@ -22,14 +21,12 @@ public class Controller {
         Set<PatchNote> curPatchNotes = crawler.crawl();
         List<String> news = patchNotesStorage.getNews(curPatchNotes);
 
-        String channelId = SecretReader.read(SecretReader.CHANNEL_ID);
-        MessageChannel channel = myJDA.getTextChannelById(channelId);
-                if (channel != null) {
-            channel.sendMessage("direct channel").queue();
-        } else {
-            System.out.println("not found");
+        MessageChannel channel = getChannel();
+        if (channel == null) {
+            System.out.println("channel not found");
+            return;
         }
-
+        channel.sendMessage("## " + "channel found!");
         if (news.isEmpty()) {
             return;
         }
@@ -37,20 +34,23 @@ public class Controller {
         for (String n : news) {
             for (PatchNote curPatchNote : curPatchNotes) {
                 if (n.equals(curPatchNote.title)) {
-                    // should parse and
-                    // bot.announceToDiscord
+                    String message = "";
 
-                    //<span class="date">2023.05.31</span> // date
-                    //<span class="line_clamp">5월 31일(수) 최신 버전 업데이트 안내 (최종 수정 : 2023-05-31 20:43)</span> // title
-                    //<a href=
-                    // "https://www.kr.playblackdesert.com/News/Notice/Detail?groupContentNo=10426&amp;countryType=ko-kr"
-                    //link
-//1113603635591520270
-                    //channel.sendMessage()
+                    // need stream
+                    message += "updated at " + curPatchNote.date + "\n";
+                    message += "# " + curPatchNote.title + "\n";
+                    message += curPatchNote.link;
 
-                    System.out.println(curPatchNote.title);
+                    channel.sendMessage(message);
                 }
             }
         }
+    }
+
+    // need optimize 매번 이짓을 하고 있음
+    public MessageChannel getChannel() {
+        String channelId = SecretReader.read(SecretReader.CHANNEL_ID);
+
+        return myJDA.getTextChannelById(channelId);
     }
 }
